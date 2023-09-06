851b721f2deb78a211f95ed9be462322c22bf098
mruqqie <mruqqie@gmail.com>
2023-09-06 23:05:46 +0100

    modified:   public/index.html 	modified:   src/App.css 	modified:   src/App.tsx 	modified:   src/components/NavBar.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,24 +8,36 @@ import {
 	VideoDetail,
 } from "./components";
 import "./App.css";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+	typography: {
+		fontFamily: ["Oswald", "sans-serif"].join(","),
+	},
+});
 
 function App() {
 	return (
-		<Box>
-			<BrowserRouter>
-				<NavBar />
-				<Routes>
-					<Route path="/channel/:id" element={<ChannelDetail />} />
-					<Route path="/" element={<Feed />} />
-					<Route
-						path="/search/:searchTerm"
-						element={<SearchFeed />}
-					/>
-					<Route path="/" element={<Feed />} />
-					<Route path="/video/:id" element={<VideoDetail />} />
-				</Routes>
-			</BrowserRouter>
-		</Box>
+		<ThemeProvider theme={theme}>
+			<Box sx={{ backgroundColor: "#000000" }}>
+				<BrowserRouter>
+					<NavBar />
+					<Routes>
+						<Route
+							path="/channel/:id"
+							element={<ChannelDetail />}
+						/>
+						<Route path="/" element={<Feed />} />
+						<Route
+							path="/search/:searchTerm"
+							element={<SearchFeed />}
+						/>
+						<Route path="/" element={<Feed />} />
+						<Route path="/video/:id" element={<VideoDetail />} />
+					</Routes>
+				</BrowserRouter>
+			</Box>
+		</ThemeProvider>
 	);
 }
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,68 @@
-import { Stack } from "@mui/material";
-import YouTubeIcon from '@mui/icons-material/YouTube';
-import MenuIcon from '@mui/icons-material/Menu';
+import { Stack, Typography, Paper, IconButton } from "@mui/material";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
-	return <Stack direction="row">
-
-	</Stack>;
+	return (
+		<Stack
+			direction="row"
+			alignItems="center"
+			sx={{
+				position: "sticky",
+				paddingLeft: 3,
+				paddingRight: 3,
+				paddingTop: 1,
+				justifyContent: "space-between",
+			}}
+		>
+			<Stack direction="row" spacing={3} alignItems="center">
+				<MenuIcon sx={{ color: "#9a9898" }} fontSize="medium" />
+				<Stack direction="row" alignItems="center">
+					<YouTubeIcon sx={{ color: "#ff0000" }} fontSize="large" />
+					<Typography
+						className="yt"
+						variant="h6"
+						sx={{ color: "#ffffff" }}
+					>
+						YouTube
+					</Typography>
+				</Stack>
+			</Stack>
+			<Stack direction="row">
+				<Paper
+					component="form"
+					onSubmit={() => {}}
+					sx={{
+						width: "400px",
+						height: "40px",
+						borderTopLeftRadius: 20,
+						borderBottomLeftRadius: 20,
+						pl: 2,
+						pr: 2,
+						border: "1px solid #4f4f4f",
+						bgcolor: "#131212",
+					}}
+				>
+					<input className="searchBar" placeholder="Search" />
+				</Paper>
+				<IconButton
+					sx={{
+						width: "70px",
+						height: "42px",
+						border: "1px solid #4f4f4f",
+						borderRadius: 0,
+						borderTopRightRadius: 20,
+						borderBottomRightRadius: 20,
+						bgcolor: "#323232",
+					}}
+				>
+					<SearchIcon sx={{ color: "#ffffff" }} />
+				</IconButton>
+			</Stack>
+		</Stack>
+	);
 };
 
 export default NavBar;
