c1f824b4678e40d31ecd49cbce4f035cc7cde29d
mruqqie <mruqqie@gmail.com>
2023-09-07 17:53:28 +0100

    modified:   src/App.css 	modified:   src/components/NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,83 +1,271 @@
-import { Stack, Typography, Paper, IconButton, TextField } from "@mui/material";
+import {
+	Stack,
+	Typography,
+	Paper,
+	IconButton,
+	useMediaQuery,
+} from "@mui/material";
 import YouTubeIcon from "@mui/icons-material/YouTube";
 import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
-	return (
-		<Stack
-			direction="row"
-			alignItems="center"
-			sx={{
-				position: "sticky",
-				paddingLeft: {
-					sm: 3,
-					xs: 1
-				},
-				paddingRight: 3,
-				paddingTop: "2px",
-				justifyContent: "space-between",
-			}}
-		>
-			<Stack direction="row" spacing={3} alignItems="center">
-				<MenuIcon sx={{ color: "#9a9898" }} fontSize="medium" />
-				<Stack direction="row" alignItems="center">
-					<YouTubeIcon sx={{ color: "#ff0000" }} fontSize="large" />
-					<Typography
-						className="yt"
-						variant="h6"
-						sx={{ color: "#ffffff" }}
+	const isXsScreen = useMediaQuery("(max-width:600px)");
+	const [showSearchInput, setShowSearchInput] = useState(false);
+	const renderSearchIcon = () => {
+		if (isXsScreen) {
+			return (
+				<Stack alignItems="center" sx={{ paddingTop: 1 }}>
+					<IconButton
+						aria-label="search"
+						size="small"
+						sx={{
+							borderRadius: 0,
+							borderTopRightRadius: 20,
+							borderBottomRightRadius: 20,
+						}}
+						onClick={() => {
+							setShowSearchInput(true);
+						}}
 					>
-						YouTube
-					</Typography>
+						<SearchIcon sx={{ color: "#ffffff" }} />
+					</IconButton>
 				</Stack>
-			</Stack>
-			<Stack direction="row" alignItems="center" sx={{ paddingTop: 1 }}>
-				<Paper
-					component="form"
-					onSubmit={() => {}}
+			);
+		} else {
+			return (
+				<Stack
+					direction="row"
+					alignItems="center"
+					sx={{ paddingTop: 1 }}
+				>
+					<Paper
+						component="form"
+						onSubmit={() => {}}
+						sx={{
+							display: "flex",
+							width: {
+								md: "382px",
+								sm: "282px",
+							},
+							height: {
+								lg: "40px",
+								md: "35px",
+								sm: "30px",
+							},
+							borderTopLeftRadius: 20,
+							borderBottomLeftRadius: 20,
+							border: "1px solid #4f4f4f",
+							bgcolor: "#131212",
+							alignContent: "center",
+						}}
+					>
+						<input className="searchBar" placeholder="Search" />
+					</Paper>
+					<IconButton
+						aria-label="search"
+						size="small"
+						sx={{
+							width: "70px",
+							height: {
+								lg: "41px",
+								md: "36px",
+								sm: "31px",
+							},
+							border: "1px solid #4f4f4f",
+							borderRadius: 0,
+							borderTopRightRadius: 20,
+							borderBottomRightRadius: 20,
+							bgcolor: "#323232",
+						}}
+					>
+						<SearchIcon sx={{ color: "#ffffff" }} />
+					</IconButton>
+				</Stack>
+			);
+		}
+	};
+
+	const renderSearchInput = () => {
+		if (showSearchInput) {
+			return (
+				<Stack
+					direction="row"
+					alignItems="center"
+					justifyContent="center"
+					sx={{ paddingTop: 1, paddingBottom: 1, bgcolor: "#212121" }}
+				>
+					<IconButton  sx={{ width: "22px", paddingRight: 2 }} onClick={()=> {setShowSearchInput(false)}}><ArrowBackIcon sx={{ color: "#9a9898" }} /></IconButton>
+					<Paper
+						component="form"
+						onSubmit={() => {}}
+						sx={{
+							display: "flex",
+							width: "70%",
+							height: "35px",
+							borderTopLeftRadius: 20,
+							borderBottomLeftRadius: 20,
+							alignContent: "center",
+							bgcolor: "#323232",
+						}}
+					>
+						<input className="searchBar" placeholder="Search" />
+					</Paper>
+					<IconButton
+						aria-label="search"
+						size="small"
+						sx={{
+							width: "70px",
+							height: "35px",
+							borderRadius: 0,
+							borderTopRightRadius: 20,
+							borderBottomRightRadius: 20,
+							bgcolor: "#323232",
+						}}
+					>
+						<SearchIcon sx={{ color: "#9a9898" }} />
+					</IconButton>
+				</Stack>
+			);
+		} else {
+			return (
+				<Stack
+					direction="row"
+					alignItems="center"
 					sx={{
-						display: "flex",
-						width: {
-							md: "382px",
-							sm: "282px"
+						position: "sticky",
+						paddingLeft: {
+							sm: 3,
+							xs: 2,
 						},
-						height: {
-							lg: "40px",
-							md: "35px",
-							sm: "30px"
+						paddingRight: {
+							sm: 3,
+							xs: 2,
 						},
-						borderTopLeftRadius: 20,
-						borderBottomLeftRadius: 20,
-						border: "1px solid #4f4f4f",
-						bgcolor: "#131212",
-						alignContent: "center",
+						paddingTop: "2px",
+						justifyContent: "space-between",
 					}}
 				>
-					<input className="searchBar" placeholder="Search" />
-				</Paper>
-				<IconButton
-					aria-label="search"
-					size="small"
-					sx={{
-						width: "70px",
-						height: {
-							lg: "41px",
-							md: "36px",
-							sm: "31px"
-						},
-						border: "1px solid #4f4f4f",
-						borderRadius: 0,
-						borderTopRightRadius: 20,
-						borderBottomRightRadius: 20,
-						bgcolor: "#323232",
-					}}
-				>
-					<SearchIcon sx={{ color: "#ffffff" }} />
-				</IconButton>
-			</Stack>
-		</Stack>
+					<Stack
+						direction="row"
+						spacing={isXsScreen ? 1 : 3}
+						alignItems="center"
+					>
+						<MenuIcon
+							sx={{ color: "#9a9898" }}
+							fontSize={isXsScreen ? "small" : "medium"}
+						/>
+						<Stack direction="row" alignItems="center">
+							<YouTubeIcon
+								sx={{ color: "#ff0000" }}
+								fontSize={isXsScreen ? "medium" : "large"}
+							/>
+							<Typography
+								className="yt"
+								variant={isXsScreen ? "body1" : "h6"}
+								sx={{ color: "#ffffff" }}
+							>
+								YouTube
+							</Typography>
+						</Stack>
+					</Stack>
+					{renderSearchIcon()}
+				</Stack>
+			);
+		}
+	};
+
+	return (
+		<div>{renderSearchInput()}</div>
+		// <Stack
+		// 	direction="row"
+		// 	alignItems="center"
+		// 	sx={{
+		// 		position: "sticky",
+		// 		paddingLeft: {
+		// 			sm: 3,
+		// 			xs: 2,
+		// 		},
+		// 		paddingRight: {
+		// 			sm: 3,
+		// 			xs: 2,
+		// 		},
+		// 		paddingTop: "2px",
+		// 		justifyContent: "space-between",
+		// 	}}
+		// >
+		// 	<Stack
+		// 		direction="row"
+		// 		spacing={isXsScreen ? 1 : 3}
+		// 		alignItems="center"
+		// 	>
+		// 		<MenuIcon
+		// 			sx={{ color: "#9a9898" }}
+		// 			fontSize={isXsScreen ? "small" : "medium"}
+		// 		/>
+		// 		<Stack direction="row" alignItems="center">
+		// 			<YouTubeIcon
+		// 				sx={{ color: "#ff0000" }}
+		// 				fontSize={isXsScreen ? "medium" : "large"}
+		// 			/>
+		// 			<Typography
+		// 				className="yt"
+		// 				variant={isXsScreen ? "body1" : "h6"}
+		// 				sx={{ color: "#ffffff" }}
+		// 			>
+		// 				YouTube
+		// 			</Typography>
+		// 		</Stack>
+		// 	</Stack>
+		// 	{renderSearchIcon()}
+		// 	<Stack direction="row" alignItems="center" sx={{ paddingTop: 1 }}>
+		// 		<Paper
+		// 			component="form"
+		// 			onSubmit={() => {}}
+		// 			sx={{
+		// 				display: "flex",
+		// 				width: {
+		// 					md: "382px",
+		// 					sm: "282px",
+		// 				},
+		// 				height: {
+		// 					lg: "40px",
+		// 					md: "35px",
+		// 					sm: "30px",
+		// 				},
+		// 				borderTopLeftRadius: 20,
+		// 				borderBottomLeftRadius: 20,
+		// 				border: "1px solid #4f4f4f",
+		// 				bgcolor: "#131212",
+		// 				alignContent: "center",
+		// 			}}
+		// 		>
+		// 			<input className="searchBar" placeholder="Search" />
+		// 		</Paper>
+		// 		<IconButton
+		// 			aria-label="search"
+		// 			size="small"
+		// 			sx={{
+		// 				width: "70px",
+		// 				height: {
+		// 					lg: "41px",
+		// 					md: "36px",
+		// 					sm: "31px",
+		// 				},
+		// 				border: "1px solid #4f4f4f",
+		// 				borderRadius: 0,
+		// 				borderTopRightRadius: 20,
+		// 				borderBottomRightRadius: 20,
+		// 				bgcolor: "#323232",
+		// 			}}
+		// 		>
+		// 			<SearchIcon sx={{ color: "#ffffff" }} />
+		// 		</IconButton>
+		// 	</Stack>
+		// </Stack>
 	);
 };
 
