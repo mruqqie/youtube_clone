676115cf113b68954ac02b733d5eff2e5be35aac
mruqqie <mruqqie@gmail.com>
2023-09-13 22:22:04 +0100

    modified:   src/components/Feed.tsx

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,4 +1,11 @@
-import { Stack, Grid, Skeleton, Box, useMediaQuery } from "@mui/material";
+import {
+	Stack,
+	Grid,
+	Skeleton,
+	Box,
+	useMediaQuery,
+	Typography,
+} from "@mui/material";
 import SideBar from "./SideBar";
 import fetchVideos, { VidApiRes } from "../api/fetchVideos";
 import React, { useEffect, useState } from "react";
@@ -18,7 +25,8 @@ const Feed = () => {
 	}, []);
 
 	const isXsScreen = useMediaQuery("(max-width:600px)");
-	console.log(data?.items.map(item => item.id));
+	console.log(data);
+	// console.log(data?.items.map(item => item.id));
 	return (
 		<Grid
 			direction="row"
@@ -35,7 +43,7 @@ const Feed = () => {
 					<SideBar />
 				</Grid>
 			)}
-
+			{/* 
 			<Grid
 				item
 				xs={12}
@@ -114,7 +122,94 @@ const Feed = () => {
 						</Box>
 					</Grid>
 				</Grid>
+			</Grid> */}
+
+			<Grid
+				item
+				xs={12}
+				sm
+				sx={{ height: { xs: "90%", sm: "100%" }, overflowY: "scroll" }}
+			>
+				<Grid
+					container
+					gap={1.5}
+					sx={{
+						display: "flex",
+						justifyContent: "center",
+						paddingLeft: "3px",
+						paddingRight: "3px",
+					}}
+				>
+					{data?.items.map((item) => (
+						<Grid
+							item
+							key={item.id}
+							sx={{
+								width: {
+									lg: "24%",
+									md: "32%",
+									sm: "48%",
+									xs: "80%",
+								},
+							}}
+						>
+							<Box
+								sx={{
+									display: "flex",
+									alignItems: "center",
+									flexDirection: "column",
+									gap: 2,
+								}}
+							>
+								<img
+									src={item.snippet.thumbnails.standard.url}
+									width="100%"
+									// sx={{
+									// 	width: "100%",
+									// 	paddingBottom: "65%",
+									// }}
+								/>
+								<Stack
+									gap={2}
+									direction="row"
+									sx={{ width: "100%" }}
+								>
+									<Skeleton
+										variant="circular"
+										width={40}
+										height={40}
+										sx={{
+											bgcolor: "grey.800",
+										}}
+									/>
+									<Stack gap={2} sx={{ width: "86%" }}>
+										<Typography
+											sx={{
+												color: "#ffffff",
+												width: "100%",
+											}}
+											variant="body2"
+										>
+											{item.snippet.localized.title}
+										</Typography>
+										<Typography
+											sx={{
+												color: "#ffffff",
+												width: "100%",
+												paddingBottom: "0%",
+											}}
+											variant="body2"
+										>
+											{item.snippet.channelTitle}
+										</Typography>
+									</Stack>
+								</Stack>
+							</Box>
+						</Grid>
+					))}
+				</Grid>
 			</Grid>
+
 			{isXsScreen && (
 				<Grid item xs={12} sm="auto">
 					<SideBar />
