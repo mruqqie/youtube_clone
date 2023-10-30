99d95d496002cda6e8fe9b800006d525c953b04e
mruqqie <mruqqie@gmail.com>
2023-10-30 09:12:51 +0100

    modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -21,16 +21,18 @@ function App() {
 		<Box sx={{ backgroundColor: "#000000" }}>
 			<BrowserRouter basename="/youtube_clone">
 				<Routes>
-					<Route
-						path="/channel/:id"
-						element={<ChannelDetail />}
-					/>
-					<Route path="/" element={<Feed />} />
-					<Route
-						path="/search/:searchTerm"
-						element={<SearchFeed />}
-					/>
-					<Route path="/video/:id" element={<VideoDetail />} />
+					<Route path="/">
+						<Route
+							path="/channel/:id"
+							element={<ChannelDetail />}
+						/>
+						<Route path="/" element={<Feed />} />
+						<Route
+							path="/search/:searchTerm"
+							element={<SearchFeed />}
+						/>
+						<Route path="/video/:id" element={<VideoDetail />} />
+					</Route>
 				</Routes>
 			</BrowserRouter>
 		</Box>
